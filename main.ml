4268c1dcee917711b7127c39bc181a179824b6be
Drew Atkin <datkin@gmail.com>
2018-10-11 18:12:52 -0400

    table with colors

--- main.ml
+++ main.ml
@@ -8,7 +8,7 @@ module App : sig
   val initial_model : Model.t
 end = struct
   module Model = struct
-    type t = unit
+    type t = int list
     let cutoff = phys_equal
   end
 
@@ -21,17 +21,40 @@ end = struct
     type t = unit
   end
 
-  let initial_model : Model.t = ()
+  let initial_model : Model.t = List.init 100 ~f:(fun _ -> Random.int 100)
 
   let apply_action (_ : Action.t) (model : Model.t) (_ : State.t) =
     model
 
   let view (model : Model.t Incr.t) ~inject:_ =
     let open Vdom in
-    let%map _model = model in
+    let%map model = model in
+    let rows =
+      List.mapi model ~f:(fun i count ->
+        let color =
+          if count < 10
+          then "green"
+          else if count < 100
+          then "blue"
+          else "red"
+        in
+        Node.tr
+          []
+          [
+            Node.td 
+              []
+              [ Node.text (Int.to_string (i + 1)) ];
+            Node.td 
+              [ Attr.style [ ("color", color) ] ]
+              [ Node.text (Int.to_string count) ];
+          ])
+    in
     Node.body
       []
-      [ Node.h3 [] [ Node.text "My app" ] ]
+      [
+        Node.h3 [] [ Node.text "My app" ];
+        Node.table [] rows;
+      ]
   ;;
 
   let on_startup ~schedule:_ (_ : Model.t) = Deferred.return ()
